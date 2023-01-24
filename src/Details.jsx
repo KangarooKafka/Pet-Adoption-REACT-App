aa91443f65983ba0fb53157d704aa123e4ae4609
Kevin Darke <99612676+KangarooKafka@users.noreply.github.com>
2023-01-24 13:21:46 -0500

    Updates to Details and SearchParams

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,8 +1,16 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useState, useContext } from 'react';
+import Modal from './Modal';
 import { useQuery } from '@tanstack/react-query';
 import fetchPet from './fetchPet';
+import Carousel from './Carousel';
+import ErrorBoundary from './ErrorBoundary';
+import AdoptedPetContext from './AdoptedPetContext';
 
 const Details = () => {
+    const [showModal, setShowModal] = useState(false);
+    const navigate = useNavigate();
+    const [_, setAdoptedPet] = useContext(AdoptedPetContext);
     const { id } = useParams();
     const results = useQuery(['details', id], fetchPet);
 
@@ -18,16 +26,43 @@ const Details = () => {
 
     return (
         <div className={'details'}>
+            <Carousel images={pet.images} />
             <div>
                 <h1>{pet.name}</h1>
                 <h2>
                     {pet.animal} - {pet.breed} - {pet.city}
                 </h2>
-                <button>Adopt {pet.name} </button>
+                <button onClick={() => setShowModal(true)}>Adopt {pet.name} </button>
                 <p>{pet.description}</p>
+                {showModal ? (
+                    <Modal>
+                        <div>
+                            <h1>Would you like to adopt {pet.name}?</h1>
+                            <div className={'buttons'}>
+                                <button
+                                    onClick={() => {
+                                        setAdoptedPet(pet);
+                                        navigate('/');
+                                    }}
+                                >
+                                    Yes
+                                </button>
+                                <button onClick={() => setShowModal(false)}>No</button>
+                            </div>
+                        </div>
+                    </Modal>
+                ) : null}
             </div>
         </div>
     );
 };
 
-export default Details;
+function DetailsErrorBoundary(props) {
+    return (
+        <ErrorBoundary>
+            <Details {...props} />
+        </ErrorBoundary>
+    );
+}
+
+export default DetailsErrorBoundary;
